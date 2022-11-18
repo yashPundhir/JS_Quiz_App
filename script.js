d485f809cbf59cde0c8428b625efcad2997787e0
yashPundhir <yash.pundhir.prof@gmail.com>
2022-11-18 23:30:48 +0530

    UI Part is done

---
+++ script.js
@@ -0,0 +1,59 @@
+const quizData = [
+	{
+		question:
+			"Which built-in method calls a function for each element in the array?",
+		a: "while()",
+		b: "loop()",
+		c: "forEach()",
+		d: "None of the above",
+		correct: "c",
+	},
+	{
+		question:
+			"Which built-in method reverses the order of the elements of an array?",
+		a: "changeOrder(order)",
+		b: "reverse()",
+		c: "sort(order)",
+		d: "None of the above",
+		correct: "b",
+	},
+	{
+		question:
+			"Which of the following is a valid type of function javascript supports?",
+		a: "named function",
+		b: "anonymous function",
+		c: "Both the above",
+		d: "None of the above",
+		correct: "c",
+	},
+	{
+		question: "What year was JavaScript launched?",
+		a: "1996",
+		b: "1995",
+		c: "1994",
+		d: "none of the above",
+		correct: "b",
+	},
+];
+
+const quiz = 0;
+const answerEls = 0;
+const questionEl = 0;
+const a_text = 0;
+const b_text = 0;
+const c_text = 0;
+const d_text = 0;
+const submitBtn = 0;
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {}
+
+function deselectAnswers() {}
+
+function getSelected() {}
+
+submitBtn.addEventListener("click", () => {});
